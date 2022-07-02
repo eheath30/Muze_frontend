fffd1a7aee06501b0a2910f856ee90c0f75c2634
eheath30 <elliot.heath@btinternet.com>
2022-07-02 16:44:17 +0100

    text counter updated

--- client/js/index.js
+++ client/js/index.js
@@ -1,11 +1,11 @@
-/*key variables 
+/*key variables
 - 1. parentsection, which is called to read emoji or comment submit button interaction
 - 2. btn, which is called to read post submit button interaction (could be included in parentSection)
 */
 const parentSection = document.querySelector('.parentsection')
 const btn = document.getElementById("submitButton");
 
-/*Event listeners 1.load content from DB on DOM load 
+/*Event listeners 1.load content from DB on DOM load
 - 2. submit post on click
 - 3. run printComment or updateReaction on event */
 document.addEventListener("DOMContentLoaded",  () => {
@@ -25,3 +25,27 @@ parentSection.addEventListener('click', (e) => {
         updateReaction(e);
       }
   })
+
+
+// Post character counter and user alert
+// collect elements
+let textArea = document.getElementById("postText");
+let characterCounter = document.getElementById("charCount");
+const maxNum = 255;
+// update char-count function
+const countCharacters = () => {
+ let numOfCharsEntered = textArea.value.length;
+ let counter = maxNum - numOfCharsEntered;
+ characterCounter.textContent = counter
+//styling changer
+ if (counter < 100) {
+  characterCounter.style.color = "red";
+}
+// alert box
+if (counter < 1) {
+    alert("Maximum number of characters Entered")
+}
+
+}
+// activation
+textArea.addEventListener("input", countCharacters);
